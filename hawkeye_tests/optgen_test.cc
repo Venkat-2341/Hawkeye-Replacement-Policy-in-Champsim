f724b8555f18385c7d49c9a472c4e450aecd788a
Venkat-2341 <vke.1743@gmail.com>
2026-09-12 13:29:05 +0000

    Tested OPTgen and verify its correctness

---
+++ hawkeye_tests/optgen_test.cc
@@ -0,0 +1,38 @@
+#include "../replacement/hawkeye/optgen.h"
+#include <iostream>
+#include <vector>
+#include <utility>
+#include <cstdint>
+
+int main() {
+    OPTgen opt(1, 2);
+
+    // TEST_VECTOR_START
+    std::vector<std::pair<std::size_t, uint64_t>> accesses = {
+        // {0, 0x1000},  // A - miss
+        // {0, 0x2000},  // B - miss
+        // {0, 0x1000},  // A - hit
+        // {0, 0x3000},  // C - miss
+        // {0, 0x1000},  // A - hit
+        // {0, 0x2000},  // B - hit
+        // {0, 0x3000},  // C - miss
+        // {0, 0x4000},  // D - miss
+        // {0, 0x1000},  // A - hit
+        // {0, 0x4000},  // D - hit
+        // {0, 0x2000},  // B - hit
+        // {0, 0x3000},  // C - miss
+    };
+    // TEST_VECTOR_END
+
+    int hits = 0;
+    for (auto& [set_idx, addr] : accesses) {
+            bool hit = opt.access(set_idx, addr);
+            std::cout << std::hex << addr << std::dec << ": " << (hit ? "HIT" : "MISS") << "\n";
+            if (hit) {
+                hits++;
+                // std::cout << std::hex << addr << std:: endl;
+            }
+    }
+
+    std::cout << "TOTAL HITS: " << hits << "\n";
+}
